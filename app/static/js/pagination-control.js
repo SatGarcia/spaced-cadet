e733ebddbb5b451f06f7b91623bdd34c10d4b370
Sat Garcia <sat@sandiego.edu>
2022-05-09 15:42:01 -0700

    add pagination to selection of topics when setting up assessments

---
+++ app/static/js/pagination-control.js
@@ -0,0 +1,71 @@
+export default {
+  compilerOptions: {
+    delimiters: ["[[", "]]"]
+  },
+
+  props: {
+    num_items: Number,
+    description: String,
+  },
+
+  emits: ['selected-page'],
+
+  computed: {
+    num_pages() {
+      return Math.ceil(this.num_items / this.items_per_page);
+    },
+
+    pages() {
+      const num_pages = Math.ceil(this.num_items / this.items_per_page);
+      let arr = [];
+      for (let i = 0; i < num_pages; i++) {
+        arr.push({start: i * this.items_per_page, end: Math.min((i+1)*this.items_per_page, this.num_items)});
+      }
+
+      return arr
+    }
+  },
+
+  data() {
+    return {
+      items_per_page: 25,
+      selected_page_num: 0,
+    }
+  },
+
+  methods: {
+    setSelectedPage(index) {
+      this.selected_page_num = index;
+      this.$emit('selected-page', this.pages[index].start, this.pages[index].end);
+    },
+
+    selectPreviousPage() {
+      this.setSelectedPage(this.selected_page_num - 1);
+    },
+
+    selectNextPage() {
+      this.setSelectedPage(this.selected_page_num + 1);
+    },
+  },
+
+  template: `
+  <nav :aria-label="description">
+    <ul class="pagination justify-content-center">
+      <li class="page-item" :class="{disabled: selected_page_num === 0}">
+        <a class="page-link" href="javascript:undefined" @click="selectPreviousPage">Previous</a>
+      </li>
+
+      <li class="page-item" v-for="(page, index) in pages" :key="index"
+          :class="{active: selected_page_num === index}">
+        <a class="page-link" href="javascript:undefined"
+            @click="setSelectedPage(index)">[[index + 1]]</a>
+      </li>
+
+      <li class="page-item" :class="{disabled: selected_page_num === (pages.length-1)}">
+        <a class="page-link" href="javascript:undefined" @click="selectNextPage">Next</a>
+      </li>
+    </ul>
+  </nav>
+  `
+
+};
