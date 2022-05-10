ef39b20a84a849b2a18267ba499c757437f1c56c
Sat Garcia <sat@sandiego.edu>
2022-05-10 14:55:24 -0700

    add pagination to 'my questions' page

--- app/static/js/pagination-control.js
+++ app/static/js/pagination-control.js
@@ -20,6 +20,22 @@ export default {
 
   emits: ['selected-page'],
 
+  watch: {
+    pages(new_pages, old_pages) {
+      if (new_pages.length === 0) {
+        // if there are now no items, 0 out page number and emit event saying
+        // no items were selected (start and end both 0)
+        this.selected_page_num = 0;
+        this.$emit('selected-page', 0, 0);
+      }
+      else if (this.selected_page_num >= new_pages.length) {
+        // if the page we were on doesn't exist anymore, switch to whatever
+        // page is the last one now
+        this.setSelectedPage(new_pages.length-1);
+      }
+    },
+  },
+
   computed: {
     num_pages() {
       return Math.ceil(this.num_items / this.items_per_page);
