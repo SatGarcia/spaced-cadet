04a8f88169a83b78ebecc8a7191312a996584bb1
Sat Garcia <sat@sandiego.edu>
2022-05-20 14:20:59 -0700

    add functions to work with course topics API; refactored a bit

--- app/static/js/cadet-api.js
+++ app/static/js/cadet-api.js
@@ -2,14 +2,7 @@ import { getCookie, fetchOrRefresh } from './helpers.js';
 
 const refresh_url = "/auth/refresh";
 
-export async function createNewObjective(description, is_public) {
-    const url = "/api/objectives";  // "{{ url_for('objectives_api') }}";
-
-    const message_body = {
-        'description': description,
-        'public': is_public,
-    }
-
+export async function postItem(url, item) {
     const config = {
         method: 'POST',
         credentials: 'same-origin',
@@ -17,10 +10,32 @@ export async function createNewObjective(description, is_public) {
             "Content-Type": "application/json",
             'X-CSRF-TOKEN': getCookie('csrf_access_token'),
         },
-        body: JSON.stringify(message_body),
+        body: JSON.stringify(item),
     };
 
     return await fetchOrRefresh(url, 'POST', refresh_url, config);
+}
+
+export async function createNewObjective(description, is_public) {
+    const url = "/api/objectives";  // "{{ url_for('objectives_api') }}";
+
+    const new_objective = {
+        'description': description,
+        'public': is_public,
+    };
+
+    return await postItem(url, new_objective);
+}
+
+
+export async function createNewTopic(text) {
+    const url = "/api/topics";  // "{{ url_for('topics_api') }}";
+
+    const new_topic = {
+        'text': text,
+    };
+
+    return await postItem(url, new_topic);
 }
 
 
@@ -70,26 +85,6 @@ export async function updateQuestionField(field_name, field_value, question_id) 
 export async function deleteQuestion(question_id) {
     const url = `/api/question/${question_id}`;
     return await fetchOrRefresh(url, 'DELETE');
-}
-
-export async function createNewTopic(text) {
-    const url = "/api/topics";  // "{{ url_for('topics_api') }}";
-
-    const message_body = {
-        'text': text,
-    }
-
-    const config = {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            "Content-Type": "application/json",
-            'X-CSRF-TOKEN': getCookie('csrf_access_token'),
-        },
-        body: JSON.stringify(message_body),
-    };
-
-    return await fetchOrRefresh(url, 'POST', refresh_url, config);
 }
 
 export async function setObjectiveTopic(objective_id, topic_id) {
@@ -143,9 +138,7 @@ export async function getCourseTextbooks(course_id) {
     return await fetchOrRefresh(url, 'GET', refresh_url);
 }
 
-export async function addCourseTextbook(course_id, textbook_id) {
-    const url = `/api/course/${course_id}/textbooks`; // FIXME: url_for
-    
+export async function addToCollection(url, item_id) {
     const config = {
         method: 'POST',
         credentials: 'same-origin',
@@ -153,10 +146,20 @@ export async function addCourseTextbook(course_id, textbook_id) {
             "Content-Type": "application/json",
             'X-CSRF-TOKEN': getCookie('csrf_access_token'),
         },
-        body: JSON.stringify({'ids': [textbook_id]})
+        body: JSON.stringify({'ids': [item_id]})
     };
 
     return await fetchOrRefresh(url, 'PUT', refresh_url, config);
+}
+
+export async function addCourseTextbook(course_id, textbook_id) {
+    const url = `/api/course/${course_id}/textbooks`; // FIXME: url_for
+    return await addToCollection(url, textbook_id);
+}
+
+export async function addCourseTopic(course_id, topic_id) {
+    const url = `/api/course/${course_id}/topics`; // FIXME: url_for
+    return await addToCollection(url, topic_id);
 }
 
 export async function removeCourseTextbook(course_id, textbook_id) {
@@ -164,8 +167,19 @@ export async function removeCourseTextbook(course_id, textbook_id) {
     return await fetchOrRefresh(url, 'DELETE', refresh_url);
 }
 
+export async function removeCourseTopic(course_id, topic_id) {
+    const url = `/api/course/${course_id}/topic/${topic_id}`; // FIXME: url_for
+    return await fetchOrRefresh(url, 'DELETE', refresh_url);
+}
+
 export async function searchTextbooks(search_string) {
     let url = "/api/textbooks/search";  // FIXME "{{ url_for('textbook_search_api') }}";
+    url = url + "?q=" + encodeURIComponent(search_string);
+    return await fetchOrRefresh(url, 'GET', refresh_url);
+}
+
+export async function searchTopics(search_string) {
+    let url = "/api/topics";  // FIXME: url_for
     url = url + "?q=" + encodeURIComponent(search_string);
     return await fetchOrRefresh(url, 'GET', refresh_url);
 }
