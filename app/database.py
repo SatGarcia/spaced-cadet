7088e66c745ffe8dbc6097436fef0d8e3984bf3a
Sat Garcia <sat@sandiego.edu>
2022-06-06 11:27:31 -0700

    update CLI command to allow adding all types of users, not just admin+instructor users

--- app/database.py
+++ app/database.py
@@ -6,25 +6,29 @@ from app import db
 from app.db_models import User, Question
 
 
-@click.command('add-admin')
+@click.command('add-user')
+@click.option('--admin', is_flag=True, default=False)
+@click.option('--instructor', is_flag=True, default=False)
 @click.argument('email')
 @click.argument('first_name')
 @click.argument('last_name')
 @with_appcontext
-def add_admin_user(email, first_name, last_name):
-    """Adds a new admin user to the database. Their password will be
-    randomized."""
+def add_admin_user(admin, instructor, email, first_name, last_name):
+    """Adds a new user to the database. Their password will be
+    randomized so they must reset their password via the website."""
 
-    admin_user = User(email=email,
+    new_user = User(email=email,
                         first_name=first_name,
                         last_name=last_name,
-                        admin=True,
-                        instructor=True)
-    admin_user.set_password(User.generate_password(30))
-    db.session.add(admin_user)
+                        admin=admin,
+                        instructor=instructor)
+    new_user.set_password(User.generate_password(30))
+    db.session.add(new_user)
     db.session.commit()
 
-    click.echo(f"Added new admin user: {first_name} {last_name} ({email}).")
+    click.echo(f"Added new user: {first_name} {last_name} ({email}).")
+    click.echo(f"\tAdmin: {admin}")
+    click.echo(f"\tInstructor: {instructor}")
 
 
 def init_app(app):
