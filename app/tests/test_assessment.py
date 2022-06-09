26390db4e1ded1749df9af8a0c4526abc5f3a53f
Alizea Hinz <ahinz@sandiego.edu>
2022-06-09 16:48:07 -0700

    added test for fresh_questions

--- app/tests/test_assessment.py
+++ app/tests/test_assessment.py
@@ -102,9 +102,59 @@ class AssessmentModelCase(unittest.TestCase):
     def test_waiting_questions(self):
         pass
 
+
+
+
+
+
     @unittest.skip("Test not implemented")
     def test_fresh_questions(self):
-        pass
+        """ Returns all "fresh" assessment questions, where fresh is defined as needing
+        to be practiced by the user for the FIRST time today. 
+        
+        questions should be unattempted, due, or overdue
+        """
+        # adds questions and users to this session 
+        a = Assessment(title="Test assessment")
+        q1 = ShortAnswerQuestion(prompt="Question 1", answer="Answer 1")
+        q2 = ShortAnswerQuestion(prompt="Question 2", answer="Answer 2")
+        q3 = ShortAnswerQuestion(prompt="Question 3", answer="Answer 3")
+        q4 = ShortAnswerQuestion(prompt="Question 4", answer="Answer 4")
+
+        db.session.add(a)
+        db.session.commit()
+        a.questions.append(q1)
+        a.questions.append(q2)
+        a.questions.append(q3)
+
+        u1 = User(email="test@test.com", first_name="Test", last_name="User")
+        u1.set_password("test")
+        u2 = User(email="test2@test.com", first_name="Test2", last_name="User")
+        u2.set_password("test")
+        db.session.add_all([u1, u2])
+        db.session.commit()
+
+        # if no questions are attempted yet, they should be fresh
+        self.assertCountEqual(a.fresh_questions(u1).all(), [q1,q2,q3,q4])
+
+        # three attempts for u1: one with next attempt of today, one with next
+        # attempt yesterday, and one with tomorrow
+        q2_attempt = TextAttempt(response="Attempt2", user=u1, question=q2,
+                                 next_attempt=date.today())
+        q3_attempt = TextAttempt(response="Attempt3", user=u1, question=q3,
+                                 next_attempt=(date.today()-timedelta(days=1))) # attempt of yesterday
+        q4_attempt = TextAttempt(response="Attempt4", user=u1, question=q4,
+                                 next_attempt=(date.today()+timedelta(days=1))) # attempt of tomorrow
+
+        # single attempt for u2, with next attempt of today to make sure
+        # method differentiates between users
+        q1_attempt = TextAttempt(response="Attempt1", user=u2, question=q1,
+                                 next_attempt=date.today())
+
+        db.session.add_all([q2_attempt, q3_attempt, q4_attempt, q1_attempt])
+
+        self.assertCountEqual(a.fresh_questions(u1).all(), [q2,q3])
+
 
     @unittest.skip("Test not implemented")
     def test_repeat_questions(self):
