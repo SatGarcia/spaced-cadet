ac904acd0ba32f837a3285a436a0dfa073e433b9
Sat Garcia <sat@sandiego.edu>
2022-06-10 14:27:20 -0700

    create new cypress test spec file with e2e tests for an instructor managing a course

---
+++ cypress/e2e/course_management.cy.js
@@ -0,0 +1,67 @@
+describe('Instructor Course Management', () => {
+  beforeEach(() => {
+    // reset and seed the database prior to every test
+    cy.request('/test/reset_db')
+
+    // seed a user in the DB that we can control from our tests
+    cy.request('POST', '/test/seed/user', { 
+      instructor: true,
+      email: 'test@cadet.com',
+      password: 'testing',
+      'first-name': 'Foo',
+      'last-name': 'Bar' })
+      .its('body')
+      .as('currentUser')
+
+    cy.request('POST', '/test/seed/course', { 
+      name: 'test-course',
+      'instructor_email': 'test@cadet.com',
+      'num_students': 5,
+      'num_past_assessments': 2,
+      'num_upcoming_assessments': 3})
+      .its('body')
+      .as('testCourse')
+
+    // login (without UI)
+    cy.request({
+      url: '/auth/login',
+      method: 'POST',
+      form: true,
+      body: {
+        email: 'test@cadet.com',
+        password: 'testing'
+      },
+    })
+
+    // our auth cookie should be present
+    cy.getCookie('access_token_cookie').should('exist')
+  })
+
+  it('Course Overview Page', function () {
+    cy.visit(`/c/${this.testCourse.name}`)
+
+    cy.get('h3').should('contain', this.testCourse.title)
+
+    cy.get('[id=instructorMenu]').contains('Manage Assessments')
+    cy.get('[id=instructorMenu]').should('contain', 'Manage Roster')
+    cy.get('[id=instructorMenu]').should('contain', 'Edit Settings')
+    cy.get('[id=instructorMenu]').should('contain', 'Course Setup')
+  })
+
+  it('Edit Course Settings Page', function () {
+    cy.visit(`/c/${this.testCourse.name}`)
+
+    cy.get('[id=instructorMenu]').contains('Edit Settings').click()
+
+    cy.contains("Edit Course Settings")
+
+    cy.get('input[name=title]').clear().type('Zippy Course Title')
+    cy.get('textarea[name=description]').clear().type('A shiny new course description')
+    cy.get('input[name=start_date]').type('2021-01-15')
+    cy.get('input[name=submit]').click()
+    cy.url().should('contain', '/c/test-course')
+    cy.get('h3').should('contain', 'Zippy Course Title')
+    cy.contains('A shiny new course description')
+  })
+})
+
