d9624700ce1e5402221ba8cd6e3d05b39ea0739a
grrissa <marissanesteban@gmail.com>
2022-06-12 17:20:46 -0700

    get past courses

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -47,10 +47,6 @@ class UserModelCase(unittest.TestCase):
                     start_date=(date.today()-timedelta(days=1)),
                                 end_date=(date.today()-timedelta(days=1)))
 
-        # to make sure that a new course isn't appended
-        db.session.add(c5)
-        db.session.commit()
-
         # add the newer course in first (just to make sure ordering is correct
         # later)
         u.courses.append(c3)
@@ -67,12 +63,63 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(current_courses, [c1, c2])
 
     def test_get_active_courses(self):
+
         u = User(email="test@test.com", first_name="Test", last_name="User")
         u.set_password('test')
         db.session.add(u)
         db.session.commit()
 
         self.assertEqual(u.get_active_courses(), [])
+
+        #start: yesterday end: tomorrow
+        c1 = Course(name="test-course1", title="Test Course 1", description="",
+                    start_date=(date.today()-timedelta(days=1)),
+                    end_date=(date.today()+timedelta(days=1)))
+
+        #start: today end: today
+        c2 = Course(name="test-course2", title="Test Course 2", description="",
+                    start_date=date.today(), end_date=date.today())
+
+        #start: tomorrow end: 2 days
+        c3 = Course(name="test-course3", title="Test Course 3", description="",
+                    start_date=(date.today()+timedelta(days=1)),
+                                end_date=(date.today()+timedelta(days=2)))
+
+        #start: yesterday end: yesterday
+        c4 = Course(name="test-course4", title="Test Course 4", description="",
+                    start_date=(date.today()-timedelta(days=1)),
+                                end_date=(date.today()-timedelta(days=1)))
+
+        c5 = Course(name="test-course5", title="Test Course 5", description="",
+                    start_date=(date.today()-timedelta(days=1)),
+                                end_date=(date.today()-timedelta(days=1)))
+
+        # to make sure that a new course isn't appended QEUSTION
+        db.session.add(c5)
+        db.session.commit()
+
+        # add the newer course in first (just to make sure ordering is correct
+        # later)
+        u.courses.append(c3)
+        u.courses.append(c2)
+        u.courses.append(c1)
+        u.courses.append(c4)
+
+        current_courses = u.get_active_courses()
+
+        # check that contents are correct (i.e. disregarding order)
+        self.assertCountEqual(current_courses, [c1, c2, c3])
+
+        # check that results are sorted from oldest start_date to newest
+        self.assertEqual(current_courses, [c1, c2, c3])
+
+    def test_get_past_courses(self):
+        u = User(email="test@test.com", first_name="Test", last_name="User")
+        u.set_password('test')
+        db.session.add(u)
+        db.session.commit()
+
+        self.assertEqual(u.get_past_courses(), [])
 
         #start: yesterday end: tomorrow
         c1 = Course(name="test-course1", title="Test Course 1", description="",
@@ -108,13 +155,13 @@ class UserModelCase(unittest.TestCase):
         u.courses.append(c1)
         u.courses.append(c4)
 
-        current_courses = u.get_active_courses()
+        current_courses = u.get_past_courses()
 
         # check that contents are correct (i.e. disregarding order)
-        self.assertCountEqual(current_courses, [c1, c2, c3])
+        self.assertCountEqual(current_courses, [c4])
 
         # check that results are sorted from oldest start_date to newest
-        self.assertEqual(current_courses, [c1, c2, c3])
+        self.assertEqual(current_courses, [c4])
 
     @unittest.skip("Test not implemented")
     def test_latest_next_attempts(self):
