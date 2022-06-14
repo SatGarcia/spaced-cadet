2c501365b5df186c0fbdb50c969bfcb4d44de0d2
Alizea Hinz <ahinz@sandiego.edu>
2022-06-13 19:41:47 -0700

    added additional courses to test_course.py

--- app/tests/test_course.py
+++ app/tests/test_course.py
@@ -10,12 +10,24 @@ class CourseModelCase(unittest.TestCase):
         self.app_context.push()
         db.create_all()
 
+        # create a test course that will remain empty to test
+        self.c_empty = Course(name="test-course-empty", title="Test Course empty", description="",
+                    start_date=(date.today()-timedelta(days=3)),
+                    end_date=(date.today()+timedelta(days=3)))
+
         # create a test course that is a week long to give room to test assessments that will be added to this course
         self.c = Course(name="test-course1", title="Test Course 1", description="",
                     start_date=(date.today()-timedelta(days=3)),
                     end_date=(date.today()+timedelta(days=3)))
+
+        # create a second test course that is a week long to give room to test assessments that will be added to this course
+        self.c2 = Course(name="test-course2", title="Test Course 2", description="",
+                    start_date=(date.today()-timedelta(days=3)),
+                    end_date=(date.today()+timedelta(days=3)))
         
         db.session.add(self.c)
+        db.session.add(self.c2)
+        db.session.add(self.c_empty)
         db.session.commit()
 
         self.a1 = Assessment(title="Test assessment1", time = datetime.now()-timedelta(days=2))  # assessment occurs during course, before current date
@@ -24,6 +36,22 @@ class CourseModelCase(unittest.TestCase):
         self.a4 = Assessment(title="Test assessment4", time = datetime.now()+timedelta(hours=1)) # assessment occurs during course, today (an hour after this moment)
         self.a5 = Assessment(title="Test assessment5", time = datetime.now()+timedelta(days=2))  # assessment occurs during course, after current date
 
+        # add assessments within the first course all with differing times
+        self.c.assessments.append(self.a5)
+        self.c.assessments.append(self.a4)
+        self.c.assessments.append(self.a3)
+        self.c.assessments.append(self.a2)
+        self.c.assessments.append(self.a1)
+
+        # extra assessments to add to a second course
+        self.a6 = Assessment(title="Test assessment6", time = datetime.now()-timedelta(hours=1)) # assessment occurs during course, today (an hour before this moment)
+        self.a7 = Assessment(title="Test assessment7", time = datetime.now()+timedelta(hours=1)) # assessment occurs during course, today (an hour after this moment)
+        
+        # add assessments within the second course: will ensure that the function can differentiate between different courses
+        self.c2.assessments.append(self.a6)
+        self.c2.assessments.append(self.a7)
+
+        db.session.commit()
 
     def tearDown(self):
         db.session.remove()
@@ -33,28 +61,16 @@ class CourseModelCase(unittest.TestCase):
 
     def test_upcoming_assessments(self):
         # test: if no assessments are added to the course yet, then none should be upcoming
-        self.assertCountEqual(self.c.upcoming_assessments(), []) 
+        self.assertCountEqual(self.c_empty.upcoming_assessments(), []) 
 
-        # add assessments within the course all with differing times. a4 and a5 should be upcoming because they haven't occurred yet
-        self.c.assessments.append(self.a5)
-        self.c.assessments.append(self.a4)
-        self.c.assessments.append(self.a3)
-        self.c.assessments.append(self.a2)
-        self.c.assessments.append(self.a1)
-
-        self.assertCountEqual(self.c.upcoming_assessments(), [self.a4,self.a5])
+        # a4 and a5 should be upcoming because they haven't occurred yet
+        self.assertCountEqual(self.c.upcoming_assessments(), [self.a4, self.a5])
 
 
     def test_past_assessments(self):
         # test: if no assessments are added to the course yet, then none should be upcoming
-        self.assertCountEqual(self.c.past_assessments(), []) 
+        self.assertCountEqual(self.c_empty.past_assessments(), []) 
 
-        # add assessments within the course all with differing times. a1,a2,a3 should be past because they have already occurred 
-        self.c.assessments.append(self.a5)
-        self.c.assessments.append(self.a4)
-        self.c.assessments.append(self.a3)
-        self.c.assessments.append(self.a2)
-        self.c.assessments.append(self.a1)
-
+        # a1,a2,a3 should be past because they have already occurred 
         self.assertCountEqual(self.c.past_assessments(), [self.a1, self.a2, self.a3])
         
