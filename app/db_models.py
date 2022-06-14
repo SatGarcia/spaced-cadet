2171a8a0d1a2532b3b7f51e8b89dbe38a6606472
Alizea Hinz <ahinz@sandiego.edu>
2022-06-14 11:26:12 -0700

    updates for test_course

--- app/db_models.py
+++ app/db_models.py
@@ -629,11 +629,12 @@ class Course(SearchableMixin, db.Model):
     def past_assessments(self):
         return self.assessments.filter(Assessment.time < datetime.now())
 
-    def upcoming_meetings(self): # wouldn't it make more sense if the course meetings were time instead of date?
-        return self.meetings.filter(ClassMeeting.date >= datetime.now()) #date.today())
+    def upcoming_meetings(self): 
+        """ make comment here"""
+        return self.meetings.filter(ClassMeeting.date >= date.today())
 
     def previous_meetings(self):
-        return self.meetings.filter(ClassMeeting.date < datetime.now()) #date.today())
+        return self.meetings.filter(ClassMeeting.date < date.today())
 
 
 class CourseSchema(Schema):
