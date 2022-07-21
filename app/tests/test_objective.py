ed5b9217bc88d23b82d4481f6e175afd3a6fde12
Alizea Hinz <ahinz@sandiego.edu>
2022-07-20 19:45:32 -0700

    wrote review_questions in the objective class and test functions for it

--- app/tests/test_objective.py
+++ app/tests/test_objective.py
@@ -104,4 +104,78 @@ class ObjectiveModelCase(unittest.TestCase):
 
         self.assertEqual(self.lo1.get_e_factor_average(self.u1), 2.633)
 
+    def test_review_questions_with_assessment(self):
+        self.a.objectives.append(self.lo1)
+        self.a.objectives.append(self.lo2)
+
+        self.a.questions.append(self.q1)
+        self.a.questions.append(self.q2)
+        self.a.questions.append(self.q3)
+        self.a.questions.append(self.q4)
+        self.a.questions.append(self.q5)
+
+        # before any attempts, there shouldn't be any in the list
+        self.assertEqual(self.lo1.review_questions(self.u1, self.a), [])
+
+        q2_attempt = TextAttempt(response="Attempt2", user=self.u1, question=self.q2,
+                                 next_attempt=date.today(),e_factor = 3.5)
+        q3_attempt = TextAttempt(response="Attempt3", user=self.u1, question=self.q3,
+                                 next_attempt=date.today(), e_factor = 1.4)
+
+        # multiple attempts for the same question, only the last one should be considered in the list
+        q4_attempt = TextAttempt(response="Attempt4", user=self.u1, question=self.q4,
+                                 next_attempt=date.today(), e_factor = 3)
+        q4_attempt2 = TextAttempt(response="Attempt4b", user=self.u1, question=self.q4,
+                                 next_attempt=date.today()+timedelta(hours=1), e_factor = 2)
+
+        # ensure differentiation between lo1 and lo2
+        q5_attempt = TextAttempt(response="Attempt5", user=self.u1, question=self.q5,
+                                 next_attempt=date.today(), e_factor = 2)
+
+        # lo1: single attempt for u2, to make sure method differentiates between users
+        q1_attempt = TextAttempt(response="Attempt1", user=self.u2, question=self.q1,
+                                 next_attempt=date.today(), e_factor = 2)
+
+
+        db.session.add_all([q2_attempt, q3_attempt, q4_attempt, q4_attempt2, q1_attempt, q5_attempt])
+
+
+        self.assertEqual(self.lo1.review_questions(self.u1, self.a), [self.q3, self.q4])
+
+        # test e_factor_threshold
+        self.assertEqual(self.lo1.review_questions(self.u1, self.a, 4), [self.q2, self.q3, self.q4])
+
+    def test_review_questions_without_assessment(self):
+        # before any attempts, there shouldn't be an average
+        self.assertEqual(self.lo1.review_questions(self.u1), [])
+
+        q2_attempt = TextAttempt(response="Attempt2", user=self.u1, question=self.q2,
+                                 next_attempt=date.today(),e_factor = 3.5)
+        q3_attempt = TextAttempt(response="Attempt3", user=self.u1, question=self.q3,
+                                 next_attempt=date.today(), e_factor = 1.4)
+
+        # multiple attempts for the same question, only the last one should be included in the average
+        q4_attempt = TextAttempt(response="Attempt4", user=self.u1, question=self.q4,
+                                 next_attempt=date.today(), e_factor = 3)
+        q4_attempt2 = TextAttempt(response="Attempt4b", user=self.u1, question=self.q4,
+                                 next_attempt=date.today(), e_factor = 2)
+
+        # ensure differentiation between lo1 and lo2
+        q5_attempt = TextAttempt(response="Attempt5", user=self.u1, question=self.q5,
+                                 next_attempt=date.today(), e_factor = 4)
+
+        # lo1: single attempt for u2, to make sure method differentiates between users
+        q1_attempt = TextAttempt(response="Attempt1", user=self.u2, question=self.q1,
+                                 next_attempt=date.today(), e_factor = 4)
+
+
+        db.session.add_all([q2_attempt, q3_attempt, q4_attempt, q4_attempt2, q1_attempt, q5_attempt])
+
+
+        self.assertEqual(self.lo1.review_questions(self.u1), [self.q3, self.q4])
+
+        # test e_factor_threshold
+        self.assertEqual(self.lo1.review_questions(self.u1, None, 4), [self.q2, self.q3, self.q4])
+
+
         
