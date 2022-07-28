3419b9dc92606d4a95288d8118a3d9e97316e2d8
Alizea Hinz <ahinz@sandiego.edu>
2022-07-28 09:48:18 -0700

    keeps second to last block

--- app/static/js/multiple-choice-form.js
+++ app/static/js/multiple-choice-form.js
@@ -45,7 +45,7 @@ function remove_mc_option() {
     let options_list = document.getElementById('mc-options');
     let last_option = options_list.lastElementChild;
     //console.log(last_option);
-    if (last_option !== options_list.firstElementChild) {
+    if (last_option.previousElementSibling !== options_list.firstElementChild) {
         options_list.removeChild(last_option);
     }
 }
